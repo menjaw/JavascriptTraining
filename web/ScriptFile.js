1f8449b31cc63baeb9eb0fe9d6a11ae81d40bea4
Menja Holm Wettergren <Menja Holm Wettergren>
2016-09-05 12:56:38 +0200

    initial commit

---
+++ web/ScriptFile.js
@@ -0,0 +1,65 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//var blueBox = document.getElementById("blueBox");
+//blueBox.onclick = function(){
+//    alert("it is working!");
+//}
+//
+//var temp1;
+//var temp2;
+//
+//myFunction1();
+//
+//function myFunction1(){
+//    temp1 = "Hallo from 1";
+//    }
+//    
+//var myFunction2 = function(){
+//    temp2 = "Hallo from 2";
+//    }
+//
+//myFunction2();
+//console.log(temp1);
+//console.log(temp2);
+
+
+//var secondLevel = 10;
+//function myFuno() {
+//    var firstLevel = 100;
+//    (function nestedFuno() {
+//        var secondLevel = 20;
+//        console.log(firstLevel); //printer nummer 100
+//    })();
+//    console.log(secondLevel); //printer nummer 10
+//}
+//myFuno();
+//person1 = {
+//    "name" : "Hans",
+//    "age" : 17,
+//    getName: function(){
+//        var fullname = this.name + this.age;
+//        return fullname;
+//    }
+//};
+//person2 = {
+//    "name" : "Hans-Jørgen",
+//    "age" : 17,
+//    getName: function(){
+//        var fullname = this.name + this.age;
+//        return fullname;
+//    }
+//};
+//persons = [person1, person2];
+//classroom = {
+//    "students": persons, 
+//    getStudents: function(){
+//        return this.students;
+//    }
+//};
+//
+//console.log(classroom.getStudents()[0].getName());
+
